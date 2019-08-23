a0c27005d3417a3fc03f6f616eda03bc95f4b61e
Carol Sanders <csanders@twistbioscience.com>
2019-08-23 14:13:09 -0700

    Setup Database

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 // database connection event
-mongoose.connection.on('connected', function () {
-  console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
+mongoose.connection.on('connected', function() {
+    console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
 });
 
 module.exports = mongoose;
