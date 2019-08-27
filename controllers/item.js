7f5872c28d3d5e9343a30c2ad4d8c0c31e72a12a
Carol Sanders <csanders@twistbioscience.com>
2019-08-27 01:07:52 -0700

    Debugging Add to Room

--- controllers/item.js
+++ controllers/item.js
@@ -9,9 +9,10 @@ module.exports = {
 
 function addToRoom(req, res) {
   Room.findById(req.params.id, function (err, room) {
+    concole.log(`INSIDE addToRoom of items.js ${req.params.id}`);
     room.item.push(req.body.itemId);
     room.save(function (err) {
-      res.redirect(`rooms/${room._id}`);
+      res.redirect(`rooms/${req.body.itemId}`);
     });
   });
 }
@@ -20,14 +21,15 @@ function create(req, res) {
   /* Creat a New item not in the list */
   Item.create(req.body, function (err, item) {
     // Item.pop(item);
-    res.redirect('/items/new');
+    res.redirect('items/new');
   });
 }
 
 function newItem(req, res) {
   /* Need to add a reference to the room the item is found in */
   Item.find({}, function (err, items) {
-    res.render('/items/new', {
+    console.log(`INSIDE newItem of items.js ${req.params.id}`);
+    res.render('items/new', {
       title: 'Items Added To Your Room',
       items
     });
