94b2e3e3aa989ce76a13d8e6e350f8e936107793
Carol Sanders <csanders@twistbioscience.com>
2019-08-29 01:05:01 -0700

    Change position of get and put id routes for updateRoom

--- routes/rooms.js
+++ routes/rooms.js
@@ -10,8 +10,8 @@ router.get('/new', roomsController.new);
 router.get('/show', roomsController.showAll);
 router.get('/:id', roomsController.show);
 router.post('/new', roomsController.create);
-router.put('/:id', roomsController.updateRoom);
 router.get('/:id', roomsController.updateRoom);
+router.put('/:id', roomsController.updateRoom);
 router.delete('/:id', roomsController.delete);
 
 module.exports = router;
